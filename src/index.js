59a169c4cde28154dcbb148d9ba72a314afb838a
ismaesousa <ismaesousa1998@outlook.com>
2019-09-20 22:32:42 -0300

    adding english comments

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 async function main(){
   /**
    * Criando lista das cidades
+   * creating list of cities
    */
 
   var cidades = [
@@ -14,6 +15,7 @@ async function main(){
   ]
   /**
    * Criando matriz com as posições das cidades
+   * Creating matrix with city positions
    */
   var matrix = new Array(cidades.length); 
   for (var i = 0; i < matrix.length; i++) {
@@ -22,6 +24,7 @@ async function main(){
 
   /**
    * Buscando da API do Google Cada distância entre duas cidades
+   * Searching the Google API Every Distance Between Two Cities
    */
   for (var i = 0; i < matrix.length; i++) {
     for (var j = 0; j < matrix.length; j++) {
@@ -29,6 +32,7 @@ async function main(){
         matrix[i][j] = 0
       } else if(i>j){
         /** Somente para não fazer requests desnecessários */
+        /** Just not to make unnecessary requests */
         matrix[i][j] = -1
       }else{
         /**
@@ -36,6 +40,11 @@ async function main(){
          * Faz a requisição
          * Desestrutura e peda o data
          * Coloca a distância na matriz
+         * 
+         * Logic to make the request
+         * Make the request
+         * Unstructures and pieces the date
+         * Put distance into matrix
          */
 
         const response = await axios
@@ -49,6 +58,7 @@ async function main(){
         })
         const {data} = response   
         /** O resultado vem 18.2 km, então eu retiro o " km" */   
+        /** The result is 18.2 km, so I remove the "km" */   
         const [kms, sigla] = data.rows[0].elements[0].distance.text.split(' ')
         
         matrix[i][j] = kms
@@ -60,6 +70,10 @@ async function main(){
    * Transforma a matriz em uma STRING para escrever no arquivo
    * No modelo cidadeA para cidadeB = Distancia
    *  Russas-Quixeré=32,7
+   * 
+   * Transforms array into STRING for writing to file
+   * In the model cityA to cityB = Distance
+   * Russian-Quixeré = 32.7
    */
   var stream = fs.createWriteStream('mapCitys.txt', {flags: 'a'});  
   for (let i = 0; i < matrix.length; i++) {   
@@ -73,6 +87,9 @@ async function main(){
 
 }
 
+/**
+ * Call funtions
+ */
 main()
 
 
